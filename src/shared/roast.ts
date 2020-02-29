7392a3a94cdb3b5933227374ac1d204f5080eca1
Roeland Schumacher <schumies@gmx.co.uk>
2020-02-29 16:51:58 +0000

    initial commit

---
+++ src/shared/roast.ts
@@ -0,0 +1,10 @@
+export interface roast {
+    id: string;
+    name: string;
+    variant: string;
+    size: number;
+    description: string;
+    starttemp: number;
+    maintemp: number;
+    intervals: number[];
+}
