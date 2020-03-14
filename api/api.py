7b51c805779f88e484bb083291045cd6f0870171
Roeland Schumacher <schumies@gmx.co.uk>
2020-03-14 17:03:48 +0000

    updated routes

--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from flask.json import jsonify
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 data_file = os.path.join(basedir, 'data.json')
@@ -11,23 +12,15 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-class Roasts(Resource):
-    def get(self):
-        with open(data_file, 'r') as f:
-            data = json.load(f)
-        return data
+@app.route('/')
+def root():
+    return 'yeah, ok'
 
-    def put(self):
-        person = '{"name": "Bob", "languages": ["English", "Fench"]}'
-        person_dict = json.loads(person)
-
-        # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-        print( person_dict)
-
-        # Output: ['English', 'French']
-        print(person_dict['languages'])
-
-api.add_resource(Roasts, '/')
+@app.route('/roasts')
+def roasts():
+    with open(data_file, 'r') as f:
+        data = json.load(f)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
